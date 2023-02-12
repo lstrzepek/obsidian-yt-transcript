7d46be08775c98d7e86b0f1955d6998de2b253a7
Łukasz Strzępek <lstrzepek@users.noreply.github.com>
2023-02-12 19:20:15 +0100

    fixed fetching url based on cursor position

--- editor-extensions.ts
+++ editor-extensions.ts
@@ -1,59 +1,22 @@
 import { Editor, EditorPosition } from "obsidian";
-
-interface WordBoundaries {
-  start: { line: number; ch: number };
-  end: { line: number; ch: number };
-}
+import { getUrlFromText } from "./url-utils";
 
 export class EditorExtensions {
-  public static getSelectedText(editor: Editor): string {
-    if (!editor.somethingSelected()) {
-      let wordBoundaries = this.getWordBoundaries(editor);
-      editor.setSelection(wordBoundaries.start, wordBoundaries.end);
-    }
-    return editor.getSelection();
-  }
+	public static getSelectedText(editor: Editor): string {
+		if (!editor.somethingSelected()) {
+			let wordBoundaries = this.getWordBoundaries(editor);
+			editor.setSelection(wordBoundaries[0], wordBoundaries[1]);
+		}
+		return editor.getSelection();
+	}
 
-  private static cursorWithinBoundaries(cursor: EditorPosition, startIndex: number, length: number): boolean {
-    let endIndex = startIndex + length;
+	private static getWordBoundaries(editor: Editor): [EditorPosition, EditorPosition] {
+		let cursor = editor.getCursor();
 
-    return startIndex <= cursor.ch && cursor.ch <= endIndex;
-  }
-
-  private static getWordBoundaries(editor: Editor): WordBoundaries {
-    let cursor = editor.getCursor();
-
-    // If its a normal URL token this is not a markdown link
-    // In this case we can simply overwrite the link boundaries as-is
-    let lineText = editor.getLine(cursor.line);
-
-    // First check if we're in a link
-    let linksInLine = lineText.matchAll(/\[([^\[\]]*)\]\((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})\)/gi);
-
-    for (let match of linksInLine) {
-      if (this.cursorWithinBoundaries(cursor, match.index ?? 0, match[0].length)) {
-        return {
-          start: { line: cursor.line, ch: match.index ?? 0 },
-          end: { line: cursor.line, ch: match.index ?? 0 + match[0].length },
-        };
-      }
-    }
-
-    // If not, check if we're in just a standard ol' URL.
-    let urlsInLine = lineText.matchAll(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi);
-
-    for (let match of urlsInLine) {
-      if (this.cursorWithinBoundaries(cursor, match.index ?? 0, match[0].length)) {
-        return {
-          start: { line: cursor.line, ch: match.index ?? 0 },
-          end: { line: cursor.line, ch: match.index ?? 0 + match[0].length },
-        };
-      }
-    }
-
-    return {
-      start: cursor,
-      end: cursor,
-    };
-  }
+		// If its a normal URL token this is not a markdown link
+		// In this case we can simply overwrite the link boundaries as-is
+		const lineText = editor.getLine(cursor.line);
+		const urlPosition = getUrlFromText(lineText, cursor.ch);
+		return [{ line: cursor.line, ch: urlPosition[0] }, { line: cursor.line, ch: urlPosition[1] }]
+	}
 }
---
+++ url-utils.test.ts
@@ -0,0 +1,32 @@
+import { getUrlFromText } from "./url-utils";
+
+describe('Should return position of the url in text line', () => {
+	test('When cursor in the middle of the url', () => {
+		expect(getUrlFromText("https://www.youtube.com/watch?v=rOSZOCoqOo8", 39))
+			.toStrictEqual([0, 43]);
+	});
+	test('When cursor at the end of the url', () => {
+		expect(getUrlFromText("https://www.youtube.com/watch?v=rOSZOCoqOo8", 43))
+			.toStrictEqual([0, 43]);
+	});
+	test('When cursor at the beginig of the url', () => {
+		expect(getUrlFromText("https://www.youtube.com/watch?v=rOSZOCoqOo8", 0))
+			.toStrictEqual([0, 43]);
+	});
+
+});
+
+describe('Should return position of the url in markdown line', () => {
+	test('When cursor in the middle of the url', () => {
+		expect(getUrlFromText("(Link)[https://www.youtube.com/watch?v=rOSZOCoqOo8]", 39))
+			.toStrictEqual([7, 51]);
+	});
+	test('When cursor at the end of the url', () => {
+		expect(getUrlFromText("(Link)[https://www.youtube.com/watch?v=rOSZOCoqOo8]", 51))
+			.toStrictEqual([7, 51]);
+	});
+	test('When cursor at the beginig of the url', () => {
+		expect(getUrlFromText("(Link)[https://www.youtube.com/watch?v=rOSZOCoqOo8]", 7))
+			.toStrictEqual([7, 51]);
+	});
+});
