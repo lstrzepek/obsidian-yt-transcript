dd08af0485eddaa278bddafeda0fe3b9a46b6f32
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-05-21 19:11:25 -0600

    refactor: move files into the src folder

--- editor-extensions.ts
+++ editor-extensions.ts
@@ -1,5 +1,5 @@
 import { Editor, EditorPosition } from "obsidian";
-import { getUrlFromText } from "./url-utils";
+import { getUrlFromText } from "./src/url-utils";
 
 export class EditorExtensions {
 	public static getSelectedText(editor: Editor): string {
@@ -10,13 +10,18 @@ export class EditorExtensions {
 		return editor.getSelection();
 	}
 
-	private static getWordBoundaries(editor: Editor): [EditorPosition, EditorPosition] {
+	private static getWordBoundaries(
+		editor: Editor
+	): [EditorPosition, EditorPosition] {
 		let cursor = editor.getCursor();
 
 		// If its a normal URL token this is not a markdown link
 		// In this case we can simply overwrite the link boundaries as-is
 		const lineText = editor.getLine(cursor.line);
 		const urlPosition = getUrlFromText(lineText, cursor.ch);
-		return [{ line: cursor.line, ch: urlPosition[0] }, { line: cursor.line, ch: urlPosition[1] }]
+		return [
+			{ line: cursor.line, ch: urlPosition[0] },
+			{ line: cursor.line, ch: urlPosition[1] },
+		];
 	}
 }
